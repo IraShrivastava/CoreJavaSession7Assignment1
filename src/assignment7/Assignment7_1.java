e05b500f3a65eff3d2829e2567a1e8b0c78a8a9a
IRA <IRA@hp>
2017-07-19 18:28:59 +0530

    Core Java Session 7 Assignment 1

---
+++ src/assignment7/Assignment7_1.java
@@ -0,0 +1,12 @@
+package assignment7;	//package declaration
+
+public class Assignment7_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=10;		//declaring an integer variable
+		String str="He is "+ String.valueOf(age) +" years old";	//converting integer variable age to string using inbuilt function
+		System.out.println(str);
+	}
+
+}
